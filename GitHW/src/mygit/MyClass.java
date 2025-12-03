01029215e717ecf27a4345c7cd543dc95fcd8e28
ogt-git <rudxor4@gmail.com>
2025-12-03 14:30:36 +0900

    수정

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,7 +4,7 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git");
-		System.out.println("2nd version");
+		System.out.println("2nd ddversion");
 		System.out.println("your change is branch master");
 		
 	}
