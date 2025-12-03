104d84008b223ba5bd420aee4e1de63034a24d3f
ogt-git <rudxor4@gmail.com>
2025-12-03 14:27:38 +0900

    fix : change in branch main

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -4,7 +4,8 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Git");
-		System.out.println("수정완료");
+		System.out.println("2nd version");
+		System.out.println("your change is branch main");
 		
 	}
 
