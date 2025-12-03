347bffcadf205222fd71d8040cb8cf220f514d5a
ogt-git <rudxor4@gmail.com>
2025-12-03 14:28:48 +0900

    fix : change branch mater

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -5,7 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Git");
 		System.out.println("2nd version");
-		System.out.println("your change is branch main");
+		System.out.println("your change is branch master");
 		
 	}
 
