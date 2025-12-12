3275a64678d70f4e2af6db63264c1bafd96301d3
ogt-git <rudxor4@gmail.com>
2025-12-12 18:49:01 +0900

    fix : 밥 수정사항

--- Gittask/src/mygit/A.java
+++ Gittask/src/mygit/A.java
@@ -4,6 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		System.out.println("A");
+		System.out.println("밥밥 디라라 라라 bob의 수정");
 
 	}
 
--- Gittask/src/mygit/B.java
+++ Gittask/src/mygit/B.java
@@ -4,6 +4,7 @@ public class B {
 
 	public static void main(String[] args) {
 		System.out.println("B");
+		System.out.println("밥밥 디라라 bob의 수정");
 
 	}
 
